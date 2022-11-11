8515641ece0dbf3f4d9cc3b4a8ad07e7b08ab80b
坂本唯斗 <sakamotoyuito@sakamotoyuitonoMacBook-Air.local>
2022-11-11 16:55:11 +0900

    APIから情報を取得する関数を作成

---
+++ src/hooks/fetcher.ts
@@ -0,0 +1,20 @@
+export const fetcher = async (apiPath: string) => {
+  const apiDomain = process.env.NEXT_PUBLIC_RESAS_API_URI
+    ? process.env.NEXT_PUBLIC_RESAS_API_URI
+    : ''
+  if (process.env.NEXT_PUBLIC_RESAS_API_KEY === undefined) {
+    throw new Error('API key is undefined.')
+  }
+  const url = apiDomain + apiPath
+  const options = {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json;charset=UTF-8',
+      'X-API-KEY': process.env.NEXT_PUBLIC_RESAS_API_KEY,
+    },
+  }
+  const res = await fetch(url, options)
+  const data = await res.json()
+  return data
+}
