32a787b0e666ff790c665ff2f3ea72e3fcb03c3c
坂本唯斗 <sakamotoyuito@sakamotoyuitonoMacBook-Air.local>
2022-11-13 00:09:47 +0900

    fetcherのテストを作成

---
+++ __tests__/hooks/fetcher.test.ts
@@ -0,0 +1,11 @@
+import { fetcher } from 'src/hooks/fetcher'
+
+describe('fetcher', () => {
+  it('apiのURLが未設定だった時エラーが発生するか', async () => {
+    expect(fetcher).rejects.toThrow()
+  })
+
+  it('apikeyが未定義だったときエラーが発生するか', async () => {
+    expect(fetcher('DAMMY')).rejects.toThrow()
+  })
+})
