0659cc63f8b537d3bc153c53fec1d2508751f4ae
坂本唯斗 <sakamotoyuito@sakamotoyuitonoMacBook-Air.local>
2022-11-13 04:38:17 +0900

    リファクタリング usePrefecturesCheckBox.ts

--- src/hooks/usePrefecturesCheckBox.ts
+++ src/hooks/usePrefecturesCheckBox.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo } from 'react';
+import { useMemo } from 'react';
 import { fetcher } from './fetcher';
 import useSWR from 'swr';
 
@@ -9,16 +9,14 @@ type PrefecturesResponses = {
 
 export const usePrefecturesCheckBox = () => {
   const { data, error } = useSWR('/api/v1/prefectures', fetcher);
-  const [prefecturesList, setPrefecturesList] = useState<string[]>(['']);
-
   if (error) {
     throw new Error('API Error /api/v1/prefectures');
   }
 
-  const prefecturesCheckBoxData = useMemo(() => {
+  const prefecturesCheckBoxData: string[] = useMemo(() => {
     if (typeof data === 'object' && 'result' in data) {
-      const prefecturesNameList = data.result.map((element: PrefecturesResponses) => {
-        const prefectureName = element.prefName;
+      const prefecturesNameList = data.result.map((response: PrefecturesResponses) => {
+        const prefectureName = response.prefName;
         return prefectureName;
       });
       return prefecturesNameList;
@@ -26,9 +24,5 @@ export const usePrefecturesCheckBox = () => {
     return [];
   }, [data]);
 
-  useEffect(() => {
-    setPrefecturesList(prefecturesCheckBoxData);
-  }, [prefecturesCheckBoxData, data]);
-
-  return prefecturesList;
+  return prefecturesCheckBoxData;
 };
