0a8aa734fae967a5f5bf07b238b7ebc1ef5c16fb
坂本唯斗 <sakamotoyuito@sakamotoyuitonoMacBook-Air.local>
2022-11-16 05:36:00 +0900

    useFormContextからデータを取得し、選択された都道府県のコードと名前の配列を返すフックを作成

---
+++ src/hooks/useSelectedPrefectures.ts
@@ -0,0 +1,31 @@
+import { useMemo } from 'react';
+import { useFormContext } from 'react-hook-form';
+import { CheckBoxData } from './usePrefecturesCheckBox';
+
+export const useSelectedPrefectures = () => {
+  const { watch } = useFormContext();
+  const checkBoxForm = watch();
+
+  const prefCodeList = useMemo(() => {
+    if (checkBoxForm && checkBoxForm.checkBox.length > 0) {
+      return checkBoxForm.checkBox
+        .filter(({ checked }: CheckBoxData) => checked === true)
+        .map(({ prefCode }: CheckBoxData) => prefCode);
+    }
+    return [];
+  }, [checkBoxForm]);
+
+  const prefNameList = useMemo(() => {
+    if (checkBoxForm && checkBoxForm.checkBox.length > 0) {
+      return checkBoxForm.checkBox
+        .filter(({ checked }: CheckBoxData) => checked === true)
+        .map(({ prefName }: CheckBoxData) => prefName);
+    }
+    return [];
+  }, [checkBoxForm]);
+
+  return {
+    prefCodeList,
+    prefNameList,
+  };
+};
