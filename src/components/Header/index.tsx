43a30e776c199394b9e48e69be284b9adceecfdd
坂本唯斗 <sakamotoyuito@sakamotoyuitonoMacBook-Air.local>
2022-11-16 05:54:38 +0900

    ヘッダーコンポーネントを作成

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css';
+
+export const HeaderComponent = () => {
+  return (
+    <header className={styles.header}>
+      <div>ゆめみフロントエンドコーディング試験アプリ</div>
+    </header>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,14 @@ import { PrefecturesCheckBoxComponent } from '@/components/CheckBox';
 import { GraphComponent } from '@/components/Graph';
 import { useFormMethods } from '@/hooks/useFormMethods';
 import { FormProvider } from 'react-hook-form';
+import { HeaderComponent } from '@/components/Header';
 
 const Home: NextPage = () => {
   const { methods } = useFormMethods();
 
   return (
     <>
+      <HeaderComponent />
       <div className={styles.container}>
         <FormProvider {...methods}>
           <PrefecturesCheckBoxComponent />
