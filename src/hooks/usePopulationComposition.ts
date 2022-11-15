d2d70c88b7c08584bfa6a5a0b044898889e2a07a
坂本唯斗 <sakamotoyuito@sakamotoyuitonoMacBook-Air.local>
2022-11-16 05:41:52 +0900

    Promise型ではなくオブジェクトを返すよう修正

--- src/hooks/usePopulationComposition.ts
+++ src/hooks/usePopulationComposition.ts
@@ -1,23 +1,42 @@
-import { useMemo } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { fetcher } from './fetcher';
 
-export const usePopulationComposition = (prefCodeList: number[]) => {
-  const populationDataFromApi = useMemo(() => {
-    if (Array.isArray(prefCodeList) && prefCodeList.length > 0) {
-      const populationData = prefCodeList.map(async (prefCode) => {
-        const fetchData = await fetcher(
-          `/api/v1/population/composition/perYear?prefCode=${prefCode}`,
-        );
-        const populationCompositionList = fetchData?.result?.data[0]?.data;
-        return {
-          code: prefCode,
-          data: populationCompositionList,
-        };
-      });
-      return populationData;
-    }
-    return [];
-  }, [prefCodeList]);
+type PopulationCompositionList = {
+  code: number;
+  name: string;
+  value: PopulationCompositionValue[];
+};
 
-  return Promise.all(populationDataFromApi);
+type PopulationCompositionValue = {
+  year: number;
+  value: number;
+};
+
+export const usePopulationComposition = (prefCodeList: number[], prefNameList: string[]) => {
+  const [response, setResponse] = useState<PopulationCompositionList[]>([]);
+  const populationDataFromApi = useCallback(async () => {
+    if (prefCodeList.length !== response.length) {
+      const populationData = await Promise.all(
+        prefCodeList.map(async (prefCode, index) => {
+          const fetchData = await fetcher(
+            `/api/v1/population/composition/perYear?prefCode=${prefCode}`,
+          );
+          const populationCompositionList: PopulationCompositionValue[] =
+            fetchData?.result?.data[0]?.data;
+          return {
+            code: prefCode,
+            name: prefNameList[index],
+            value: populationCompositionList,
+          };
+        }),
+      );
+      setResponse(populationData);
+    }
+  }, [prefCodeList, prefNameList, response]);
+
+  useEffect(() => {
+    populationDataFromApi();
+  }, [populationDataFromApi]);
+
+  return response;
 };
