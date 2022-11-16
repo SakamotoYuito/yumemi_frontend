60d09221d2e949e7c01c6cf4504e594e79057cbe
坂本唯斗 <sakamotoyuito@sakamotoyuitonoMacBook-Air.local>
2022-11-17 08:33:05 +0900

    都道府県未選択時に「都道府県を選択してください。」を出すように修正

--- src/components/Graph/index.tsx
+++ src/components/Graph/index.tsx
@@ -15,41 +15,47 @@ import { useColor } from 'src/hooks/useColor';
 
 export const GraphComponent = () => {
   const { prefCodeList, prefNameList } = useSelectedPrefectures();
-  const graphDataEachYear = useGraphDataShaping(prefCodeList, prefNameList);
+  const graphData = useGraphDataShaping(prefCodeList, prefNameList);
   const colorList = useColor();
 
   return (
     <div className={styles.graphContainer}>
-      <ResponsiveContainer width='100%' height='100%'>
-        <LineChart
-          id='test'
-          data={graphDataEachYear}
-          margin={{ top: 15, right: 20, left: 20, bottom: 15 }}
-        >
-          <CartesianGrid strokeDasharray='3 3' />
-          <XAxis
-            dataKey='year'
-            label={{ value: '年', offset: -7, position: 'insideBottomRight' }}
-          />
-          <YAxis
-            tickFormatter={(t) => String(t / 1000)}
-            label={{ value: '人口(万人)', offset: -10, angle: -90, position: 'insideLeft' }}
-          />
-          <Tooltip />
-          <Legend height={20} />
-          {prefNameList.map((name: string, index: number) => {
-            return (
-              <Line
-                key={index}
-                type='monotone'
-                dataKey={name}
-                stroke={colorList[index]}
-                activeDot={{ r: 5 }}
-              />
-            );
-          })}
-        </LineChart>
-      </ResponsiveContainer>
+      {prefCodeList.length === 0 ? (
+        <div className={styles.zero}>
+          <p>都道府県を選択してください。</p>
+        </div>
+      ) : (
+        <ResponsiveContainer width='100%' height='100%'>
+          <LineChart
+            id='test'
+            data={graphData}
+            margin={{ top: 15, right: 20, left: 20, bottom: 15 }}
+          >
+            <CartesianGrid strokeDasharray='3 3' />
+            <XAxis
+              dataKey='year'
+              label={{ value: '年', offset: -7, position: 'insideBottomRight' }}
+            />
+            <YAxis
+              tickFormatter={(t) => String(t / 1000)}
+              label={{ value: '人口(万人)', offset: -10, angle: -90, position: 'insideLeft' }}
+            />
+            <Tooltip />
+            <Legend height={20} />
+            {prefNameList.map((name: string, index: number) => {
+              return (
+                <Line
+                  key={index}
+                  type='monotone'
+                  dataKey={name}
+                  stroke={colorList[index]}
+                  activeDot={{ r: 5 }}
+                />
+              );
+            })}
+          </LineChart>
+        </ResponsiveContainer>
+      )}
     </div>
   );
 };
