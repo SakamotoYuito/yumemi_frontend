63b5d05c2f5dbf0e74111d1639f6d2201cfa9a1f
坂本唯斗 <sakamotoyuito@sakamotoyuitonoMacBook-Air.local>
2022-11-17 09:32:07 +0900

    リファクタリング: タイトルにh1タグを使用

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import styles from './styles.module.css';
 export const HeaderComponent = () => {
   return (
     <header className={styles.header}>
-      <div>ゆめみフロントエンドコーディング試験アプリ</div>
+      <h1>ゆめみフロントエンドコーディング試験アプリ</h1>
     </header>
   );
 };
